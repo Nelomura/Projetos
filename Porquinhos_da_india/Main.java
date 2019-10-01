b8001f451e441aa0235ef838c32f867f66b66ada
Nelomura <43865457+Nelomura@users.noreply.github.com>
2019-10-01 20:03:41 -0300

    Add files via upload

---
+++ Porquinhos_da_india/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class Main{
+
+    public static void main(String Args[]){
+        
+        Form janela=new Form();
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
